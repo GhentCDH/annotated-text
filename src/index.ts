983e7defbbc545a3a016405878ece4ce23472b42
Frederic Lamsens <frederic.lamsens@ugent.be>
2024-09-17 11:41:45 +0000

    updated exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * from "./components";
 export * from "./types";
 
+export { UpdateAnnotationState, CreateAnnotationState, UserActionState, UserState } from "./lib/annotatedTextUtils/StateClasses";
+
 import "./style/style.scss";
