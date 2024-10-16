baef7bd681a548f7268c2b8cf9e8e32b8aed7f2b
Bo Vandersteene <bo.vandersteene@ugent.be>
2024-10-16 14:51:08 +0200

    release: create release script

---
+++ tools/release.mjs
@@ -0,0 +1,22 @@
+import { readFileSync } from "fs";
+import { execSync } from "node:child_process";
+
+// Read input parameters
+const args = process.argv.slice(2);
+const allowedReleaseTypes = ["major", "minor", "patch"];
+const releaseType = args[0]?.substring(2);
+
+if (!allowedReleaseTypes.includes(releaseType))
+  throw new Error(
+    "Invalid release type. Allowed values are major, minor, patch"
+  );
+
+execSync(`npm version ${releaseType}`, { stdio: "inherit" });
+
+// Read version from package.json
+const packageJson = JSON.parse(readFileSync("package.json", "utf8"));
+const version = packageJson.version;
+
+// Output the parameters and version
+console.log(`Release type: ${releaseType}`);
+console.log(`New version: ${version}`);
