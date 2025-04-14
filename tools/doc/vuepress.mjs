831fb817e1ee5e460261b3c6000d38cd8871e709
Bo Vandersteene <bo@firepeak.tech>
2025-04-14 14:28:31 +0200

    fix: docs build

--- tools/doc/vuepress.mjs
+++ tools/doc/vuepress.mjs
@@ -117,6 +117,6 @@ const copyReadme = (from, to, depth = Number.MAX_SAFE_INTEGER) => {
 
 copyReadme('', '', 1);
 
-// createMenu('components');
-// createMenu('demo');
-// createMenu('api', ['_media', 'globals.md']);
+createMenu('components');
+createMenu('demo');
+createMenu('api', ['_media', 'globals.md']);
