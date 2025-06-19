07b2cb5cefc7bff95d7ea15ca7551ecce5555477
Bo Vandersteene <bo@firepeak.tech>
2025-06-19 18:13:20 +0200

    update exports

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,4 @@ export type { AnnotationColor } from "./types/AnnotationColor";
 export * from "./utils/createAnnotationColor";
 export { default as AnnotatedTextV2 } from "./components/AnnotatedTextV2.vue";
 export * from "./compute/index";
+export * from "./compute/events";
