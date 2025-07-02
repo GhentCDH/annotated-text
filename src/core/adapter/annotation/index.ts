38111a1f40b6fc8bfdc48e42162c65093188e6aa
Bo Vandersteene <bo@firepeak.tech>
2025-07-02 16:40:29 +0200

    feat: conditional color annotations

--- src/core/adapter/annotation/index.ts
+++ src/core/adapter/annotation/index.ts
@@ -3,4 +3,4 @@ export * from "./w3c/model";
 export * from "./AnnotationAdapter";
 export * from "./DefaultAnnotationAdapter";
 export * from "./w3c/W3CAnnotationAdapter";
-export { ColorFn } from "./DefaultAnnotationColor";
+export { type ColorFn } from "./DefaultAnnotationColor";
