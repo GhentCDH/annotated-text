af8eff358c674927eb9d425ae9fac01973e534de
Bo Vandersteene <bo@firepeak.tech>
2025-07-02 11:44:49 +0200

    feat: add some documentation

--- src/core/compute/create/CreateAnnotations.model.ts
+++ src/core/compute/create/CreateAnnotations.model.ts
@@ -6,19 +6,54 @@ import {
 } from "../../adapter/annotation";
 import { EventListenerType } from "../../events/event.listener";
 
+/**
+ * Create annotation is a factory function that creates an annotation model.
+ * Use this model to manage annotations and text, every change related to the visualization should be done through this function.
+ */
 export type CreateAnnotations<ANNOTATION> = {
+  /**
+   * Set the text for the model. The adapter will parse the text to the internal model.
+   * @param text
+   * @param redraw
+   */
   setText: (text: string, redraw?: boolean) => CreateAnnotations<ANNOTATION>;
+  /**
+   * Set the annotations for the model. The adapter will parse the annotations to the internal model
+   * @param annotations
+   * @param redraw If true, the annotations will be redrawn.
+   */
   setAnnotations: (
     annotations: ANNOTATION[],
     redraw?: boolean,
   ) => CreateAnnotations<ANNOTATION>;
+  /**
+   * Highlight annotations by their IDs.
+   * @param ids
+   */
   highlightAnnotations: (ids: string[]) => CreateAnnotations<ANNOTATION>;
+  /**
+   * A list of selected annotations, if this is handled outside the model,
+   * @param ids
+   */
   selectAnnotations: (ids: string[]) => CreateAnnotations<ANNOTATION>;
+  /**
+   * Register an event listener for the annotation model.
+   * @param event
+   * @param callback
+   */
   on: (
     event: EventListenerType,
     callback: EventCallback,
   ) => CreateAnnotations<ANNOTATION>;
+  /**
+   * Register an error callback that will be called when an error occurs in the annotation model.
+   * @param callback
+   */
   onError: (callback: ErrorEventCallback) => CreateAnnotations<ANNOTATION>;
+  /**
+   * Destroy the annotation model and all associated resources.
+   * Important: this will remove all event listeners and the SVG element from the DOM.
+   */
   destroy: () => CreateAnnotations<ANNOTATION>;
   /**
    * Change the configuration of the textadapter. If needed, it will recreate the annotation model.
