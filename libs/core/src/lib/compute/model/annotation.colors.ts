a5ca93537bc04fdcbc14b43ed4c394bb50519ec1
Bo Vandersteene <bo@firepeak.tech>
2025-09-03 11:35:27 +0200

    fix: #102 changing active annotation should reset the default state

--- libs/core/src/lib/compute/model/annotation.colors.ts
+++ libs/core/src/lib/compute/model/annotation.colors.ts
@@ -9,26 +9,40 @@ export class AnnotationColors {
   constructor() {}
 
   public highlightAnnotations(ids: AnnotationId[], svgModel: SvgModel) {
+    const oldIds = new Set(this.highlightedIds);
     this.highlightedIds.clear();
     ids.forEach((id) => this.highlightedIds.add(id));
 
     this.color(svgModel);
+    this.resetColors(oldIds, svgModel);
     return this;
   }
 
   public selectAnnotations(ids: AnnotationId[], svgModel: SvgModel) {
+    const oldIds = new Set(this.activeIds);
     this.activeIds.clear();
     ids.forEach((id) => this.highlightedIds.delete(id));
     ids.forEach((id) => this.activeIds.add(id));
 
     this.color(svgModel);
+    this.resetColors(oldIds, svgModel);
     // TODO decide which one has more priority?
     return this;
   }
 
+  public resetColors(
+    ids: Set<AnnotationId> | AnnotationId[],
+    svgModel: SvgModel,
+  ) {
+    ids.forEach((id) => svgModel.resetAnnotationColor(id));
+    return this;
+  }
+
   public color(svgModel: SvgModel) {
-    this.highlightedIds.forEach((id) => svgModel.resetAnnotationColor(id));
-    this.activeIds.forEach((id) => svgModel.resetAnnotationColor(id));
+    this.resetColors(this.highlightedIds, svgModel).resetColors(
+      this.activeIds,
+      svgModel,
+    );
     return this;
   }
 
