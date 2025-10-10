36302d94b786eb40c7e85cb7c8c6853ad1017c9c
Bo Vandersteene <bo@firepeak.tech>
2025-10-10 09:58:43 +0200

    chore: update build process

--- libs/core/eslint.config.mjs
+++ libs/core/eslint.config.mjs
@@ -4,18 +4,7 @@ export default [
   ...baseConfig,
   {
     files: ["**/*.json"],
-    rules: {
-      "@nx/dependency-checks": [
-        "error",
-        {
-          ignoredFiles: [
-            "{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}",
-            "{projectRoot}/rollup.config.{js,ts,mjs,mts,cjs,cts}",
-            "{projectRoot}/vite.config.{js,ts,mjs,mts}",
-          ],
-        },
-      ],
-    },
+    rules: {},
     languageOptions: {
       parser: await import("jsonc-eslint-parser"),
     },
