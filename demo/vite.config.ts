ed74697d6b42dbbb3a99b0e35c01db05dda85c91
Bo Vandersteene <bo@firepeak.tech>
2025-10-23 14:12:19 +0200

    chore: update dependencies

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types='vitest' />
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+// import vue from "@vitejs/plugin-vue";
 import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 import { nxCopyAssetsPlugin } from "@nx/vite/plugins/nx-copy-assets.plugin";
 
@@ -15,7 +15,11 @@ export default defineConfig(() => ({
     port: 4300,
     host: "localhost",
   },
-  plugins: [vue(), nxViteTsPaths(), nxCopyAssetsPlugin(["*.md"])],
+  plugins: [
+    //vue(),
+    nxViteTsPaths(),
+    nxCopyAssetsPlugin(["*.md"]),
+  ],
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
