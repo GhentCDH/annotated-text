942da56dea3a49454ce0d8610b9dc60edfe71456
Bo Vandersteene <bo@firepeak.tech>
2025-11-28 14:27:54 +0100

    docs: add custom render docs

--- docs/demo/examples/custom-annotation-renderer.ts
+++ docs/demo/examples/custom-annotation-renderer.ts
@@ -1,12 +1,23 @@
 import {
+  AnnotationRender,
+  AnnotationRenderParams,
   clearAnnotatedTextCache,
   createAnnotatedText,
+  createTextAnnotationRender,
+  DefaultUnderlineAnnotationRenderStyle,
+  getColorsUnderline,
   GutterAnnotationRender,
   HighlightAnnotationRender,
+  TextAnnotation,
   UnderLineAnnotationRender,
 } from "@ghentcdh/annotated-text";
 import { annotationColors } from "../data/const";
 import { DemoAnnotation, DemoAnnotationConfig } from "../data/data.types";
+import {
+  createAnnotationFill,
+  createAnnotationPathFn,
+  PathParams,
+} from "../../../libs/core/src/lib/compute/utils/create-path";
 
 const annotations = [
   {
@@ -39,16 +50,147 @@ const text = `This is an underline annotation
 this is a highlight annotation
 this is a gutter annotation`;
 
-export const customAnnotationRender = (id_default: string) => {
-  clearAnnotatedTextCache();
-  const activeAnnotations = [];
+const createUnderlineWithCaps: createAnnotationPathFn = (
+  params: PathParams,
+) => {
+  const fill = createAnnotationFill(params);
+  const { x, y, height, width, leftBorder, rightBorder } = params;
 
-  // TODO add example
+  const path = [];
+  const capHeight = 10;
+  const baseline = y + height;
+
+  if (leftBorder) {
+    path.push(`M ${x} ${baseline - capHeight} v ${capHeight}`);
+  }
+
+  path.push(`M ${x} ${baseline} h ${width}`);
+
+  if (rightBorder) {
+    path.push(`M ${x + width} ${baseline - capHeight} v ${capHeight}`);
+  }
+
+  return {
+    border: path.join(" "),
+    fill,
+  };
+};
+
+// Create the custom renderer class
+export class MyUnderLineAnnotationRenderer extends AnnotationRender<any> {
+  readonly weightOrder: number = 1;
+  readonly isGutter: boolean = false;
+
+  static instance = "my-underline-renderer";
+  readonly name = MyUnderLineAnnotationRenderer.instance;
+
+  constructor() {
+    super(DefaultUnderlineAnnotationRenderStyle);
+  }
+
+  createDraws(
+    params: AnnotationRenderParams,
+    parentDimensions: { x: number; y: number },
+    annotation: TextAnnotation,
+  ) {
+    return createTextAnnotationRender(
+      params,
+      this.style,
+      parentDimensions,
+      annotation,
+      createUnderlineWithCaps,
+      getColorsUnderline,
+    );
+  }
+}
+
+const createWavesPath: createAnnotationPathFn = (params: PathParams) => {
+  const { x, y: baseY, width, height } = params;
+  const waveCount = 4;
+  const amplitude = 4;
+  const y = baseY + height;
+  const waveWidth = width / waveCount;
+  const path: string[] = [`M ${x} ${y}`];
+
+  for (let i = 0; i < waveCount; i++) {
+    const direction = i % 2 === 0 ? -1 : 1;
+    const startX = x + i * waveWidth;
+    const x1 = startX + waveWidth * 0.25;
+    const y1 = y + amplitude * direction;
+    const x2 = startX + waveWidth * 0.75;
+    const y2 = y + amplitude * direction;
+    const endX = startX + waveWidth;
+
+    path.push(`C ${x1} ${y1}, ${x2} ${y2}, ${endX} ${y}`);
+  }
+
+  return {
+    border: path.join(" "),
+    fill: null,
+  };
+};
+
+// Create the custom renderer class
+export class WavesAnnotationRenderer extends AnnotationRender<any> {
+  readonly weightOrder: number = 1;
+  readonly isGutter: boolean = false;
+
+  static instance = "my-waves-renderer";
+  readonly name = WavesAnnotationRenderer.instance;
+
+  constructor() {
+    super(DefaultUnderlineAnnotationRenderStyle);
+  }
+
+  createDraws(
+    params: AnnotationRenderParams,
+    parentDimensions: { x: number; y: number },
+    annotation: TextAnnotation,
+  ) {
+    return createTextAnnotationRender(
+      params,
+      this.style,
+      parentDimensions,
+      annotation,
+      createWavesPath,
+      getColorsUnderline,
+    );
+  }
+}
+
+export const customAnnotationRender = (id_default: string, waves?: boolean) => {
+  clearAnnotatedTextCache();
+
+  createAnnotatedText<DemoAnnotation>(id_default, {
+    annotation: {
+      ...DemoAnnotationConfig,
+      render: {
+        defaultRenderer: waves
+          ? WavesAnnotationRenderer.instance
+          : MyUnderLineAnnotationRenderer.instance,
+      },
+    },
+  })
+    .registerRenders(
+      new MyUnderLineAnnotationRenderer(),
+      new WavesAnnotationRenderer(),
+    )
+    .setText(text)
+    .setAnnotations(
+      [
+        annotations,
+        {
+          id: "multi line",
+          start: 21,
+          end: 40,
+        } as DemoAnnotation,
+        { id: "next to last", start: 51, end: 55 } as DemoAnnotation,
+      ].flat(),
+    );
 };
 
 export const annotationRender = (id_default: string) => {
   clearAnnotatedTextCache();
-  const activeAnnotations = [];
 
   createAnnotatedText<DemoAnnotation>(id_default, {
     annotation: {
@@ -64,6 +206,5 @@ export const annotationRender = (id_default: string) => {
       new UnderLineAnnotationRender(),
     )
     .setText(text)
-    .setAnnotations(annotations)
-    .highlightAnnotations(activeAnnotations);
+    .setAnnotations(annotations);
 };
