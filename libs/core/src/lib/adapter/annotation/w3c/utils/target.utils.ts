944ed6c5c462a3b97440b913a3b1fc9b295988ff
Bo Vandersteene <bo@firepeak.tech>
2026-02-05 10:46:33 +0100

    fix: make target type text optional

--- libs/core/src/lib/adapter/annotation/w3c/utils/target.utils.ts
+++ libs/core/src/lib/adapter/annotation/w3c/utils/target.utils.ts
@@ -1,4 +1,4 @@
-import type { W3CAnnotation, W3CAnnotationTarget } from '../model';
+import { TargetSelector, TextPositionSelector, W3CAnnotation, W3CAnnotationTarget } from '../model';
 
 export const getTarget = (annotation: W3CAnnotation): W3CAnnotationTarget[] => {
   if (!annotation.target) return [];
@@ -6,6 +6,12 @@ export const getTarget = (annotation: W3CAnnotation): W3CAnnotationTarget[] => {
   return Array.isArray(annotation.target)
     ? annotation.target
     : [annotation.target];
+};
+
+const getTargetSelectors = (target: W3CAnnotationTarget) => {
+  if (!target.selector) return [];
+
+  return Array.isArray(target.selector) ? target.selector : [target.selector];
 };
 
 export const findSourceInTargets = (sourceUri: string) => {
@@ -23,23 +29,31 @@ export const hasSourceInTargets = (sourceUri: string) => {
   };
 };
 
-export const findTextPositionSelector = (sourceUri?: string) => {
-  return (annotation: W3CAnnotation): W3CAnnotationTarget | undefined => {
-    const targets = getTarget(annotation);
+const findSelectorByType = <TYPE extends TargetSelector>(type: string) => {
+  return (sourceUri?: string) => {
+    return (annotation: W3CAnnotation): TYPE | undefined => {
+      const targets = getTarget(annotation);
 
-    for (const target of targets) {
-      if (sourceUri && target.source !== sourceUri) {
-        break;
-      }
+      for (const target of targets) {
+        if (sourceUri && target.source !== sourceUri) {
+          break;
+        }
 
-      for (const selector of target.selector) {
-        if (selector.type === 'TextPositionSelector') {
-          return selector;
+        for (const selector of getTargetSelectors(target)) {
+          if (selector.type === type) {
+            return selector as TYPE;
+          }
         }
       }
-    }
-    return undefined;
+      return undefined;
+    };
   };
+};
+
+export const findTextPositionSelector = (sourceUri?: string) => {
+  return findSelectorByType<TextPositionSelector>('TextPositionSelector')(
+    sourceUri,
+  );
 };
 
 export const hasSameFields = <T>(
