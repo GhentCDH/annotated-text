dd23d908b12cb4eae264a45ec11f23a5cd59b550
Bo Vandersteene <bo@firepeak.tech>
2026-02-12 09:15:39 +0100

    fix: text should be on top of annotation

--- libs/core/e2e/playground/playground.ts
+++ libs/core/e2e/playground/playground.ts
@@ -1,12 +1,7 @@
 import { text } from './text';
 import { annotations } from './annotations';
 import { renderDemoDiv, writeToLogDiv } from '../_utils/render-demo';
-import {
-  type Annotation,
-  type AnnotationEventType,
-  createAnnotatedText,
-  W3CAnnotationAdapter,
-} from '../../src';
+import { type Annotation, type AnnotationEventType, createAnnotatedText } from '../../src';
 
 const mainContainer = 'playground';
 
@@ -17,26 +12,10 @@ const writeLog = (event: AnnotationEventType, a: Annotation) => {
   writeToLogDiv(textId, `${event}: ${a.start}-${a.end}`);
 };
 
-const annotatedText = createAnnotatedText(textId, {
-  text: { textOffset: 1 },
-  // annotation: {
-  //   tagConfig: {
-  //     enabled: false,
-  //     tagFn: (a: any) => (a.renderer ? '' : a.label || 'new one'),
-  //   },
-  //   edit: false,
-  //   render: {
-  //     renderFn: (a: any) => a.renderer,
-  //   },
-  //   style: {
-  //     styleFn: (a: any) => ({
-  //       color: createAnnotationColor(a.color),
-  //     }),
-  //   },
-  // },
-  annotation: W3CAnnotationAdapter({
-    sourceUri: 'aade11b7-4685-47b9-928c-240de2adcece',
-  }),
-})
+const annotatedText = createAnnotatedText(textId)
+  .setAnnotationAdapter({
+    edit: true,
+    create: true,
+  })
   .setText(text)
   .setAnnotations(annotations);
